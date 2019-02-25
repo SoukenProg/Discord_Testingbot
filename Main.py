bc5aa4ade6a26d2b45e15b9e1a42534b168560d5
Souken521 <west.haru6555@gmail.com>
2019-02-25 15:58:38 +0900

    Add Comment

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import get_env
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -11,18 +12,20 @@ async def on_ready():
     print('------')
 
 
+# "おはよう"に返答
 @client.event
 async def on_message(message):
     if message.content.startswith("おはよう") and client.user != message.author:
-
         m = "おはようございます" + message.author.name + "さん！"
         await client.send_message(message.channel, m)
 
 
+# "メンションに返答"
 @client.event
 async def on_message(message):
     if client.user.id in message.content:
         reply = f'{message.author.mention} やっほー'
         await client.send_message(message.channel, reply)
+
 
 client.run(get_env.DISCORD_TOKEN)
